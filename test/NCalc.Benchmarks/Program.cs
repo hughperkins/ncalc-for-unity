1e849a7174abe052e52674957bf95c363dd1053f
Hugh Perkins <hughperkins@gmail.com>
2024-09-06 08:11:26 -0400

    remove bunch of superfouvso files

--- test/NCalc.Benchmarks/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using BenchmarkDotNet.Running;
-using NCalc.Benchmarks;
-
-BenchmarkRunner.Run<LogicalExpressionFactoryBenchmark>(null, args);
-BenchmarkRunner.Run<SimpleEvaluationBenchmark>(null, args);
-BenchmarkRunner.Run<EvaluateVsLambdaBenchmark>(null, args);
